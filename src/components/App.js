ca3cbf66ec4c84d9accbff33417ab4fc6fc39410
Sarel <Sarelk@gmail.com>
2021-12-02 14:02:53 +0200

    Smoke test for connecting front to blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Web3 from 'web3';
+import Token from '../abis/Token.json'
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const fetchData = async () => {
+    const web3 = new Web3(window.ethereum)
+    const accounts = await web3.eth.getAccounts()
+    const abi = Token.abi
+    const networkId = await web3.eth.net.getId()
+    const networkAddress = Token.networks[networkId].address
+    const token = new web3.eth.Contract(Token.abi, networkAddress)
+    const totalSupply = await token.methods.totalSupply().call()
+    console.log(totalSupply)
+  }
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+    } else {
+        console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+    } else {
+        console.log("No authorized account found")
+    }
+}
+
+  useEffect(()=> {
+    fetchData()
+  })
   return (
     <div>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
