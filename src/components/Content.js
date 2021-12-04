560961e8eeda960a04819f9c753032e20cb3924c
Sarel <Sarelk@gmail.com>
2021-12-04 14:22:11 +0200

    Orderbook UI

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { exchangeSelector } from '../store/selectors'
+import { exchangeSelector, orderBook } from '../store/selectors'
 import { loadAllOrders } from '../store/interactions'
 import Trades from '../components/Trades'
+import OrderBook from './OrderBook'
 
 class Content extends Component {
   componentWillMount() {
@@ -35,17 +36,7 @@ class Content extends Component {
             </div>
           </div>
         </div>
-        <div className="vertical">
-          <div className="card bg-dark text-white">
-            <div className="card-header">
-              Card Title
-            </div>
-            <div className="card-body">
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="/#" className="card-link">Card link</a>
-            </div>
-          </div>
-        </div>
+        <OrderBook/>
         <div className="vertical-split">
           <div className="card bg-dark text-white">
             <div className="card-header">
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -6,7 +6,7 @@ import {
   exchangeLoaded,
   cancelledOrdersLoaded,
   filledOrdersLoaded,
-  OrdersLoaded
+  allOrdersLoaded
 } from './actions'
 import Token from '../abis/Token.json'
 import Exchange from '../abis/Exchange.json'
@@ -57,15 +57,39 @@ export const loadExchange = async (web3, networkId, dispatch) => {
 }
 
 export const loadAllOrders = async (exchange, dispatch) => {
-  const cancelStream = await exchange.getPastEvents('Cancel', { fromBlock: 0, toblock: 'latest'})
-  const cancelledOrders = cancelStream.map((event)=> event.returnValues)
+  // Fetch cancelled orders with the "Cancel" event stream
+  const cancelStream = await exchange.getPastEvents('Cancel', { fromBlock: 0, toBlock: 'latest' })
+  // Format cancelled orders
+  const cancelledOrders = cancelStream.map((event) => event.returnValues)
+  // Add cancelled orders to the redux store
   dispatch(cancelledOrdersLoaded(cancelledOrders))
-  const filledStream = await exchange.getPastEvents('Trade', { fromBlock: 0, toblock: 'latest'})
-  const filledOrders = filledStream.map((event)=> event.returnValues)
-  dispatch(filledOrdersLoaded(filledOrders))
-  const orderStream = await exchange.getPastEvents('Order', { fromBlock: 0, toblock: 'latest'})
-  const Orders = orderStream.map((event)=> event.returnValues)
-  dispatch(OrdersLoaded(Orders))
 
-  //FETCH all ORDERS
+  // Fetch filled orders with the "Trade" event stream
+  const tradeStream = await exchange.getPastEvents('Trade', { fromBlock: 0, toBlock: 'latest' })
+  // Format filled orders
+  const filledOrders = tradeStream.map((event) => event.returnValues)
+  // Add cancelled orders to the redux store
+  dispatch(filledOrdersLoaded(filledOrders))
+
+  // Load order stream
+  const orderStream = await exchange.getPastEvents('Order', { fromBlock: 0,  toBlock: 'latest' })
+  // Format order stream
+  const allOrders = orderStream.map((event) => event.returnValues)
+  // Add open orders to the redux store
+  dispatch(allOrdersLoaded(allOrders))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
