ac6f712051e12caec2902f19239b1d757540b951
Sarel <Sarelk@gmail.com>
2021-12-08 17:01:55 +0200

    Deployed contracts to testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const HDwalletProvider = require('truffle-hdwallet-provider-privkey')
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
 
@@ -9,6 +11,18 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    kovan: {
+      networkCheckTimeout: 10000,
+      provider: function() {
+        return new HDwalletProvider(
+          privateKeys.split(','),
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+        gas: 5000000,
+        gasPrice: 25000000000,
+        network_id: "42"
     }
   },
 
